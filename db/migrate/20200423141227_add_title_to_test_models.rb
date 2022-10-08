66b9d40a88db4a2ab91eb34d8490324a1c8097ad
Pavel Kalashnikov <kalashnikovisme@gmail.com>
2022-10-09 00:18:10 +0400

    Update 20200423141227_add_title_to_test_models.rb

--- db/migrate/20200423141227_add_title_to_test_models.rb
+++ db/migrate/20200423141227_add_title_to_test_models.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class AddTitleToTestModels < ActiveRecord::Migration[6.0]
+class AddTitleToTestModels < ActiveRecord::Migration[5.1]
   def change
     add_column :test_models, :title, :text
   end
